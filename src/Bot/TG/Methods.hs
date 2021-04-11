4b7a2ece396f19b1b5714066d48edd59e303a3d1
DenDragonB <dendragon@mail.ru>
2021-04-11 16:56:31 +0300

    rebuilding

---
+++ src/Bot/TG/Methods.hs
@@ -0,0 +1,57 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Bot.TG.Methods where
+
+import           Data.Aeson (encode)
+import qualified Data.ByteString.UTF8 as BS
+import qualified Data.ByteString.Lazy as BSLazy (toStrict)
+import           Network.HTTP.Simple
+
+import           Bot.TG.Types
+
+-- Types for creating requests
+data ReqSet = ReqSet {method :: String, reqParams :: [(BS.ByteString,Maybe BS.ByteString)]}
+
+getResponseFromAPI :: String -> ReqSet -> IO BS.ByteString
+getResponseFromAPI token settings = do
+    let request
+            = setRequestMethod (BS.fromString "GET")
+            $ setRequestHost   (BS.fromString "api.telegram.org")
+            $ setRequestPort   (443)
+            $ setRequestSecure (True)
+            $ setRequestPath   (BS.fromString $ "/bot" ++ token ++ "/" ++ method settings)
+            $ setRequestQueryString (reqParams settings)
+            $ defaultRequest
+    res <- httpBS request
+    return (getResponseBody res)
+
+-- Methods of Telegram API
+getUpdates :: Integer -> Integer -> ReqSet
+getUpdates timeout offset = ReqSet {method = "getUpdates",
+    reqParams = [ (BS.fromString "timeout", Just $ BS.fromString $ show timeout)
+                , (BS.fromString "offset",  Just $ BS.fromString $ show offset)
+                ] }
+
+sendMessage :: Integer -> String -> Bool -> ReqSet
+sendMessage chatID text isKB =  ReqSet {method = "sendMessage",
+                reqParams = [ (BS.fromString "chat_id", Just $ BS.fromString $ show chatID)
+                            , (BS.fromString "text", Just $ BS.fromString $ text)
+                            ] <> addKB isKB}
+
+addKB :: Bool -> [(BS.ByteString,Maybe BS.ByteString)]
+addKB isKB = 
+    [(BS.fromString "reply_markup", Just $ BSLazy.toStrict $ encode keyboard) | isKB]
+
+keyboard :: TGReplyKeyboardMarkup
+keyboard = TGKeyBoard 
+                [[TGButton "1",TGButton "2",TGButton "3",TGButton "4",TGButton "5"]]
+                True
+                True
+                False
+
+tgCopyMessage :: Integer -> Integer -> Integer -> ReqSet
+tgCopyMessage chatID fromChatID messageID = ReqSet {method = "copyMessage",
+            reqParams = [ (BS.fromString "chat_id", Just $ BS.fromString $ show chatID)
+                        , (BS.fromString "from_chat_id", Just $ BS.fromString $ show fromChatID)
+                        , (BS.fromString "message_id", Just $ BS.fromString $ show messageID)
+                        ] }                                    
